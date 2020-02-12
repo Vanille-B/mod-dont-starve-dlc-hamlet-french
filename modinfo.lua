cf2e7a24b473adc556388d04d285c7d8c336bac9
Vanille <beernaert.v@gmail.com>
2020-02-12 23:56:21 +0100

    modinfo.lua
    version 1.0.0

---
+++ modinfo.lua
@@ -0,0 +1,15 @@
+name = "Hamlet Français 1.0"
+description = "Traduction francaise de Don't Starve comprenant le DLC - Hamlet. \n Rapport ou modifs traductions : mods@goldenmood.com \n Mise à jour : 1.0 - 55% - 20-02-11"
+author = "nyu.xalis & goldenmood"
+version = "1.0"
+forumthread = ""
+restart_required = false
+
+api_version = 6
+
+icon_atlas = "Icon_Hamlet_french.xml"
+icon = "Icon_Hamlet_french.tex"
+
+dont_starve_compatible = true
+reign_of_giants_compatible = true
+hamlet_compatible = true
